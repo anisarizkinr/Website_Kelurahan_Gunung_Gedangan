6a16edf4858293457e856cdfc0de49eab4113ccc
Shintya77 <shintyarahma77@gmail.com>
2023-07-28 10:08:20 +0700

    halaman login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,7 +1,6 @@
 @extends('layouts.app')
-
 @section('content')
-<section class="login p-fixed d-flex text-center bg-primary common-img-bg">
+<section class="login p-fixed d-flex text-center bg-light">
     <!-- Container-fluid starts -->
     <div class="container">
         <div class="row">
@@ -9,54 +8,75 @@
                 <!-- Authentication card start -->
                 <div class="login-card card-block auth-body mr-auto ml-auto">
                     <form class="md-float-material">
-                        <div class="text-center">
-                            <img src="assets/images/auth/logo-dark.png" alt="logo.png">
+                        <div class="text-center-bold">
+                            <h3 class="text-center txt-primary">Admin Website Kelurahan Kedundung</h3>
                         </div>
                         <div class="auth-box">
+                            {{-- <div class="col-lg-7 mb-5 mb-lg-0 order-lg-2">
+                                <div class="img-about dots">
+                                  <img src="images/pengaduan.jpg" alt="Image" class="img-fluid" />
+                                </div>
+                               
+                              </div> --}}
                             <div class="row m-b-20">
                                 <div class="col-md-12">
-                                    <h3 class="text-left txt-primary">Sign In</h3>
+                                    <h3 class="text-left txt-primary">Login</h3>
                                 </div>
                             </div>
                             <hr/>
-                            <div class="input-group">
-                                <input type="email" class="form-control" placeholder="Your Email Address">
-                                <span class="md-line"></span>
-                            </div>
-                            <div class="input-group">
-                                <input type="password" class="form-control" placeholder="Password">
-                                <span class="md-line"></span>
-                            </div>
-                            <div class="row m-t-25 text-left">
-                                <div class="col-sm-7 col-xs-12">
-                                    <div class="checkbox-fade fade-in-primary">
-                                        <label>
-                                            <input type="checkbox" value="">
-                                            <span class="cr"><i class="cr-icon icofont icofont-ui-check txt-primary"></i></span>
-                                            <span class="text-inverse">Remember me</span>
-                                        </label>
-                                    </div>
+                                <div class="card-body">
+                                    <form method="POST" action="{{ route('login') }}">
+                                        @csrf
+                
+                                        <div class="row mb-3">
+                                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Username') }}</label>
+                
+                                            <div class="col-md-6">
+                                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                
+                                                @error('email')
+                                                    <span class="invalid-feedback" role="alert">
+                                                        <strong>{{ $message }}</strong>
+                                                    </span>
+                                                @enderror
+                                            </div>
+                                        </div>
+                
+                                        <div class="row mb-3">
+                                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
+                
+                                            <div class="col-md-6">
+                                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+                
+                                                @error('password')
+                                                    <span class="invalid-feedback" role="alert">
+                                                        <strong>{{ $message }}</strong>
+                                                    </span>
+                                                @enderror
+                                            </div>
+                                        </div>
+                
+                                        <div class="row mb-3">
+                                            <div class="col-md-6 offset-md-4">
+                                                <div class="form-check">
+                                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+                
+                                                    <label class="form-check-label" for="remember">
+                                                        {{ __('Remember Me') }}
+                                                    </label>
+                                                </div>
+                                            </div>
+                                        </div>
+                
+                                        <div class="row mb-0">
+                                            <div class="col-md-8 offset-md-4">
+                                                <button type="submit" class="btn btn-primary">
+                                                    {{ __('Login') }}
+                                                </button>
+                                            </div>
+                                        </div>
+                                    </form>
                                 </div>
-                                <div class="col-sm-5 col-xs-12 forgot-phone text-right">
-                                    <a href="auth-reset-password.html" class="text-right f-w-600 text-inverse"> Forgot Your Password?</a>
-                                </div>
-                            </div>
-                            <div class="row m-t-30">
-                                <div class="col-md-12">
-                                    <button type="button" class="btn btn-primary btn-md btn-block waves-effect text-center m-b-20">Sign in</button>
-                                </div>
-                            </div>
-                            <hr/>
-                            <div class="row">
-                                <div class="col-md-10">
-                                    <p class="text-inverse text-left m-b-0">Thank you and enjoy our website.</p>
-                                    <p class="text-inverse text-left"><b>Your Authentication Team</b></p>
-                                </div>
-                                <div class="col-md-2">
-                                    <img src="assets/images/auth/Logo-small-bottom.png" alt="small-logo.png">
-                                </div>
-                            </div>
-
                         </div>
                     </form>
                     <!-- end of form -->
