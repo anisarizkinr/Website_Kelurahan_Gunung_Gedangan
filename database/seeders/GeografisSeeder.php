03a4d8bb2f9467725f790093b1601632ec067c5b
anisarizkinr <nurrahma9b07.sooko1@gmail.com>
2023-09-19 15:48:56 +0700

    edit_geografisseeder

--- database/seeders/GeografisSeeder.php
+++ database/seeders/GeografisSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 
+
+
+use Illuminate\Support\Facades\DB;
 class GeografisSeeder extends Seeder
 {
     /**
@@ -13,6 +16,27 @@ class GeografisSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('geografis')->insert(
+            [
+
+                [
+                    'jumlah_penduduk' => '16095',
+                    'keterangan' => 'Jumlah Penduduk/Jiwa',
+                ], 
+                [
+                    'jumlah_penduduk' => '8179',
+                    'keterangan' => 'Jumlah Penduduk Laki-Laki/Jiwa',
+                ],
+                [
+                    'jumlah_penduduk' => '7916',
+                    'keterangan' => 'Jumlah Penduduk Perempuan/Jiwa',
+                ],
+                [
+                    'jumlah_penduduk' => '5308',
+                    'keterangan' => 'Jumlah Kepala Keluarga/Jiwa',
+                ]
+             
+            ]
+        );
     }
 }
